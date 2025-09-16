db85b5546bf0d5e5a05b12e82c7adb320c1162d3
Guilherme Gouvea <202503418179@alunos.estacio.br>
2025-09-16 00:57:41 +0000

    Criação da lógica para exibição dos resultados pedida no exercício e uma outra lógica tentando exibir corretamente as vitórias da carta 1, da carta 2 e os empates. A lógica do exercício está finalizada e a lógica bonus ainda vou pensar melhor para ver se há como exibir os empates por atributo sem usar comandos condicionais.

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -2,20 +2,20 @@
 
 int main() {
    
-    int tur1 = 1, tur2 = 2, resultado;                                                                     //Aqui defino todas as variáveis do tipo int
-    unsigned long int pop1 = 3000, pop2 = 2;  
-    float area1 = 65000.0, area2= 2.00, pib1 = 1.0, pib2 = 2.0, denpop1, denpop2, pibpc1, pibpc2;   //Aqui defino todas as variáveis do tipo float
-    float super1, super2;
+    /*
+    int tur1, tur2, resultado;                                                                     //Aqui defino todas as variáveis do tipo int
+    unsigned long int pop1, pop2;  
+    float area1, area2, pib1, pib2, denpop1, denpop2, pibpc1, pibpc2;   //Aqui defino todas as variáveis do tipo float
+    double super1, super2;
     
     denpop1 = (float) pop1 / area1;										            //calcula a densidade populacional da cidade da carta 1 e armazena na variável denpop1. Uso conversão explicita para que o resultado seja float.
     printf("densidade: %f\n", denpop1);
     super1 = 1.0 / denpop1;
     printf("Densidade inversa: %f\n", super1);
+    */
 
-
-    /*
     char estado1[15], estado2[15], codigo1[5], codigo2[5], cidade1[15], cidade2[15];    //Aqui defino todas as variáveis do tipo char
-    int tur1, tur2, resultado;                                                          //Aqui defino todas as variáveis do tipo int
+    int tur1, tur2;                                                          //Aqui defino todas as variáveis do tipo int
     unsigned long int pop1, pop2;  
     float area1, area2, pib1, pib2, denpop1, denpop2, pibpc1, pibpc2;                   //Aqui defino todas as variáveis do tipo float
     double super1, super2;                                                              //declarei as variáveis de super poder como double pois quando as coloco como float e o resultado do pib per capita fica na casa dos bilhões o programa falha em mostrar o valor correto do super poder.
@@ -44,8 +44,8 @@ int main() {
     scanf("%d", &tur1);                                                                 //lê o dado digitado no teclado e armazena na variavel correspondente.
     
     denpop1 = (float) pop1 / area1;											            //calcula a densidade populacional da cidade da carta 1 e armazena na variável denpop1. Uso conversão explicita para que o resultado seja float.    
-    pibpc1 = (float) pib1 / pop1;								        //calcula o PIP per capita da carta 1 e armazena na variával pibpc1. Uso conversão explicita para que o resultado seja float.
-    super1 = (double) pop1 + area1 + pib1 + tur1 + (1 / denpop1) + pibpc1;                 //calcula o supperpoder da carta 1 fazendo a conversão explicita para float
+    pibpc1 = (float) (pib1 * 1000000000) / pop1;								        //calcula o PIP per capita da carta 1 e armazena na variával pibpc1. Uso conversão explicita para que o resultado seja float.
+    super1 = (double) pop1 + area1 + (pib1 * 1000000000) + tur1 + (1 / denpop1) + pibpc1;                 //calcula o supperpoder da carta 1 fazendo a conversão explicita para float
     
     printf("\nDADOS DA CARTA 1 ARMAZENADOS COM SUSSECO!\n\n\n");                        //informa ao usuário que os dados da primeira carta foram armazenados corretamente. 
     printf("Digite os dados da Carta 2\n\n");                                           //informa que começará a coletar os dados da segunda carta.
@@ -73,7 +73,7 @@ int main() {
     
     denpop2 = (float) pop2 / area2; 												    //calcula a densidade populacional da cidade da carta 2 e armazena na variável denpop2. Uso conversão explicita para que o resultado seja float.    
     pibpc2 = (float) (pib2 * 1000000000) / pop2;									    //calcula o PIP per capita da carta 2 e armazena na variával pibpc2. Uso conversão explicita para que o resultado seja float.
-    super2 = (double) pop2 + area2 + pib2 + tur2 + (1 / denpop2) + pibpc2;                 //calcula o supperpoder da carta 1 fazendo a conversão explicita para float
+    super2 = (double) pop2 + area2 + (pib2 * 1000000000) + tur2 + (1 / denpop2) + pibpc2;                 //calcula o supperpoder da carta 1 fazendo a conversão explicita para float
     
     printf("\nDADOS DA CARTA 2 ARMAZENADOS COM SUSSECO!\n");
     printf("AS CARTAS CADASTRADAS SÃO AS SEGUINTES:\n\n\n");
@@ -88,7 +88,7 @@ int main() {
     printf("Pontos turísticos: %d\n", tur1);
     printf("Densidade populacional: %.2f habitantes por km²\n", denpop1);
     printf("PIB per capita: R$ %.2f por habitante\n", pibpc1);
-    printf("Superpoder: %.2f\n\n", super2);
+    printf("Superpoder: %.2f\n\n", super1);
     
     printf("Carta 2:\n\n");                            								//Exibição dos dados da carta 1.
     printf("Estado: %s\n", estado2);
@@ -100,19 +100,74 @@ int main() {
     printf("Pontos turísticos: %d\n", tur2);
     printf("Densidade populacional: %.2f habitantes por km²\n", denpop2);
     printf("PIB per capita: R$ %.2f por habitante\n", pibpc2);
-    printf("Superpoder: %.2f\n\n", super1);
+    printf("Superpoder: %.2f\n\n", super2);
 
-    printf("A CARTA VENCEDORA DE CADA CATEGORIA É A SEGUINTE:\n\n\n");
+    printf("A CARTA VENCEDORA DE CADA CATEGORIA É A SEGUINTE:\n\n");
+
+    //ATENÇÃO: devido a limitação dos comandos utilizados, quando os dois valores são iguais esse código falha mostrando que a carta 1 não venceu e leva a crer que a carta 2 venceu.
+    printf("População: a carta 1 venceu? %d\n", pop1 > pop2);
+    printf("Área: a carta 1 venceu? %d\n", area1 > area2);
+    printf("PIB: a carta 1 venceu? %d\n", pib1 > pib2);
+    printf("Pontos turisticos: a carta 1 venceu? %d\n", tur1 > tur2);
+    printf("Densidade Populacional: a carta 1 venceu? %d\n", (1 / denpop1) > (1 / denpop2));
+    printf("PIB par capita: a carta 1 venceu? %d\n", pibpc1 > pibpc2);
+    printf("Superpoder: a carta 1 venceu? %d\n", super1 > super2);
+    
+    
+    /*
+    //Bônus: Desenvolvi uma lógica, sem usar condicionais, para informar qual carta venceu cada comparação e o placar final.
+    //ATENÇÃO: consegui mostrar quantas vitórias de cada carte e quantos empates, mas quando os valores são iguais não consegui colocar que houve empate naquele atributo.
+    
+    int resultado, vit1, empate;
 
     resultado = pop1 < pop2;
     resultado++;
     printf("População: VENCEU A CARTA %d!\n", resultado);
+    
+    resultado = area1 < area2;        
+    resultado++;
     printf("Área: VENCEU A CARTA %d!\n", resultado);
+    
+    resultado = pib1 < pib2;    
+    resultado++;
     printf("PIB: VENCEU A CARTA %d!\n", resultado);
+    
+    resultado = tur1 < tur2;    
+    resultado++;
     printf("Pontos turísticos: VENCEU A CARTA %d!\n", resultado);
+    
+    resultado = denpop1 < denpop2;    
+    resultado++;
     printf("Densidade populacional: VENCEU A CARTA %d!\n", resultado);
+    
+    resultado = pibpc1 < pibpc2;    
+    resultado++;
     printf("PIB per capita: VENCEU A CARTA %d!\n", resultado);
-    printf("Superpoder: VENCEU A CARTA %d!\n", resultado);
+    
+    resultado = super1 < super2;    
+    resultado++;
+    printf("Superpoder: VENCEU A CARTA %d!\n\n", resultado);
+
+    vit1 = pop1 < pop2;
+    vit1 = vit1 + (area1 > area2);
+    vit1 = vit1 + (pib1 > pib2);
+    vit1 = vit1 + (tur1 > tur2);
+    vit1 = vit1 + (denpop1 > denpop2);
+    vit1 = vit1 + (pibpc1 > pibpc2);
+    vit1 = vit1 + (super1 > super2);
+    empate = (pop1 == pop2);
+    empate = empate + (area1 == area2);
+    empate = empate + (pib1 == pib2);
+    empate = empate + (tur1 == tur2);
+    empate = empate + (denpop1 == denpop2);
+    empate = empate + (pibpc1 == pibpc2);
+    empate = empate + (super1 == super2);
+    
+    printf("*****RESULTADO FINAL*****\n\n");
+    printf("A CARTA 1 VENCEU %d.\n", vit1);
+    printf("A CARTA 2 VENCEU %d.\n\n", 7 - vit1 - empate);
+    printf("HOUVERAM %d EMPATES.\n\n", empate);
     */
+  
     return 0;
 }
